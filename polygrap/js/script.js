585b00f23a5caf6d2f641a1107692f1bb74a27e0
maykilz <33904619+maykilz@users.noreply.github.com>
2018-12-05 14:41:11 +0300

    Add files via upload

---
+++ polygrap/js/script.js
@@ -0,0 +1,14 @@
+let menu  = document.getElementById('headeritems');
+let menuopen = document.getElementById('openmenu')
+let closemenu = document.getElementById('closemenu');
+
+console.log(menu);
+
+menuopen.addEventListener('click', function()  {
+ menu.style.display = "flex";
+})
+
+closemenu.addEventListener('click', function() {
+ menu.style.display =  "none";
+})
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJzY3JpcHQuanMiXSwic291cmNlc0NvbnRlbnQiOlsibGV0IG1lbnUgID0gZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoJ2hlYWRlcml0ZW1zJyk7XHJcbmxldCBtZW51b3BlbiA9IGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKCdvcGVubWVudScpXHJcbmxldCBjbG9zZW1lbnUgPSBkb2N1bWVudC5nZXRFbGVtZW50QnlJZCgnY2xvc2VtZW51Jyk7XHJcblxyXG5jb25zb2xlLmxvZyhtZW51KTtcclxuXHJcbm1lbnVvcGVuLmFkZEV2ZW50TGlzdGVuZXIoJ2NsaWNrJywgZnVuY3Rpb24oKSAge1xyXG4gbWVudS5zdHlsZS5kaXNwbGF5ID0gXCJmbGV4XCI7XHJcbn0pXHJcblxyXG5jbG9zZW1lbnUuYWRkRXZlbnRMaXN0ZW5lcignY2xpY2snLCBmdW5jdGlvbigpIHtcclxuIG1lbnUuc3R5bGUuZGlzcGxheSA9ICBcIm5vbmVcIjtcclxufSkiXSwiZmlsZSI6InNjcmlwdC5qcyJ9
