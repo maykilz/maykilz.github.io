481073293feb1df43b8a8260d8b21758fc96e3c2
maykilz <33904619+maykilz@users.noreply.github.com>
2019-02-11 23:30:27 +0300

    цу

---
+++ work/js/script.js
@@ -0,0 +1,24 @@
+
+$('.reviews__slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: false,
+  autoplaySpeed: 2000,
+  dots: true,
+  mobileFirst: true,
+  arrows: false,
+  dots: false,
+  responsive: [
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow:3,
+        slidesToShow:3,
+        arrows: true,
+        dots: true,
+      }
+    }
+  ]
+
+});
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJzY3JpcHQuanMiXSwic291cmNlc0NvbnRlbnQiOlsiXHJcbiQoJy5yZXZpZXdzX19zbGlkZXInKS5zbGljayh7XHJcbiAgc2xpZGVzVG9TaG93OiAxLFxyXG4gIHNsaWRlc1RvU2Nyb2xsOiAxLFxyXG4gIGF1dG9wbGF5OiBmYWxzZSxcclxuICBhdXRvcGxheVNwZWVkOiAyMDAwLFxyXG4gIGRvdHM6IHRydWUsXHJcbiAgbW9iaWxlRmlyc3Q6IHRydWUsXHJcbiAgYXJyb3dzOiBmYWxzZSxcclxuICBkb3RzOiBmYWxzZSxcclxuICByZXNwb25zaXZlOiBbXHJcbiAgICB7XHJcbiAgICAgIGJyZWFrcG9pbnQ6IDU3NixcclxuICAgICAgc2V0dGluZ3M6IHtcclxuICAgICAgICBzbGlkZXNUb1Nob3c6MyxcclxuICAgICAgICBzbGlkZXNUb1Nob3c6MyxcclxuICAgICAgICBhcnJvd3M6IHRydWUsXHJcbiAgICAgICAgZG90czogdHJ1ZSxcclxuICAgICAgfVxyXG4gICAgfVxyXG4gIF1cclxuXHJcbn0pOyJdLCJmaWxlIjoic2NyaXB0LmpzIn0=
