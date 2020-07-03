6b116e0b08baf78c7c67fd92d1703ac1853ba093
maykilz <33904619+maykilz@users.noreply.github.com>
2020-07-03 17:14:59 +0300

    Add files via upload

---
+++ domstroi/js/main.js
@@ -0,0 +1,42 @@
+ 
+var elements = document.getElementsByClassName('phoneinput');
+for (var i = 0; i < elements.length; i++) {
+  new IMask(elements[i], {
+    mask: '+{7}(000)000-00-00',
+  });
+}
+
+
+
+
+$(document).ready(function(){
+ 
+  $("#body").on("click","a", function (event) {
+
+      event.preventDefault();
+
+      var id  = $(this).attr('href'),
+
+          top = $(id).offset().top;
+
+      $('body,html').animate({scrollTop: top}, 1500);
+
+  }); 
+});
+
+
+
+ 
+
+
+$(document).ready(function(){
+ 
+  $(".vkblock").on("click","a", function (event) {
+    window.open('https://vk.com/maykalaliev');
+
+
+  }); 
+});
+
+
+
