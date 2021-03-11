f26ec22eca5146d3bf98f3a69997f0d8ce91f9e4
maykilz <33904619+maykilz@users.noreply.github.com>
2021-03-11 14:04:39 +0300

    Add files via upload

---
+++ Cataliztor/js/main.js
@@ -0,0 +1,8 @@
+var elements = document.getElementsByClassName('phone-input');
+for (var i = 0; i < elements.length; i++) {
+  new IMask(elements[i], {
+    mask: '+{7}(000)000-00-00',
+  });
+}
+
+ 
