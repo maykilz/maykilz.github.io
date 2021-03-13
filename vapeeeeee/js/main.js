056fe1cccb52c9af6b218cb54341cbc6b75b35cb
maykilz <33904619+maykilz@users.noreply.github.com>
2021-03-13 18:55:09 +0300

    Add files via upload

---
+++ vapeeeeee/js/main.js
@@ -0,0 +1,8 @@
+var elements = document.getElementsByClassName('form-number');
+for (var i = 0; i < elements.length; i++) {
+  new IMask(elements[i], {
+    mask: '+{7}(000)000-00-00',
+  });
+}
+
+ 
